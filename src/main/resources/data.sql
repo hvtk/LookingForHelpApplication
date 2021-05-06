f58f268dcaa2e8fe4275e86082266a997a2e71e7
hvtk <hvtk1968@gmail.com>
2021-05-06 16:49:59 +0200

    added the values for problem and takeAction in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,39 @@
+insert into problem (page) values ('Children');
+insert into problem (page) values ('Adults');
+insert into problem (page) values ('Elderly');
+insert into problem (page) values ('Youth');
+insert into problem (page) values ('sign_in');
+insert into problem (page) values ('sign_up');
+insert into problem (page) values ('home');
+insert into problem (page) values ('after_search_query');
+
 insert into problem (age_group) values ('Children');
 insert into problem (age_group) values ('Adults');
 insert into problem (age_group) values ('Elderly');
 insert into problem (age_group) values ('Youth');
 
+insert into problem (problem_name) values ('Self-confidence');
+insert into problem (problem_name) values ('Stressed-out');
+insert into problem (problem_name) values ('Worry-about');
+insert into problem (problem_name) values ('Quilty-feelings');
+insert into problem (problem_name) values ('Loneliness');
+insert into problem (problem_name) values ('Fear');
+insert into problem (problem_name) values ('Panic');
+insert into problem (problem_name) values ('Depression');
+insert into problem (problem_name) values ('Addiction');
+insert into problem (problem_name) values ('Burnout');
+insert into problem (problem_name) values ('Suicide');
+insert into problem (problem_name) values ('Abdominal-pains');
+insert into problem (problem_name) values ('Headache');
+insert into problem (problem_name) values ('Constant-tiredness');
+insert into problem (problem_name) values ('Palpitations');
+insert into problem (problem_name) values ('Intestinal-complains');
+insert into problem (problem_name) values ('Sore-muscles-and-joints');
+insert into problem (problem_name) values ('Dometic-violence');
+insert into problem (problem_name) values ('Child-abuse');
+insert into problem (problem_name) values ('Violence');
+
 insert into take_action (take_action_name) values ('Testing');
 insert into take_action (take_action_name) values ('Activities');
+insert into take_action (take_action_name) values ('Research-information');
+insert into take_action (take_action_name) values ('Can-do-it-yourself');
