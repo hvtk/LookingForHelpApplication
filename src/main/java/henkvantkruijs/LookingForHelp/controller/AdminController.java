400d82311150a7ed557f43f45b2af3e1e628cb23
hvtk <hvtk1968@gmail.com>
2021-05-10 15:56:20 +0200

    jwt token works. yeeh

--- src/main/java/henkvantkruijs/LookingForHelp/controller/AdminController.java
+++ src/main/java/henkvantkruijs/LookingForHelp/controller/AdminController.java
@@ -4,11 +4,13 @@ import henkvantkruijs.LookingForHelp.model.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/admin")
 public class AdminController {
 
     @GetMapping(value = "")
