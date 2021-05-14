b8f6b83100178ee42ef1b97226ab78761896deed
hvtk <hvtk1968@gmail.com>
2021-05-14 16:11:22 +0200

    added @RequestMapping(value = /login)

--- src/main/java/henkvantkruijs/LookingForHelp/controller/CustomersController.java
+++ src/main/java/henkvantkruijs/LookingForHelp/controller/CustomersController.java
@@ -2,8 +2,10 @@ package henkvantkruijs.LookingForHelp.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +15,17 @@ public class CustomersController {
     @GetMapping(value = "")
     public ResponseEntity<Object> getMessage() {
         return new ResponseEntity<>("SECURED REST endpoint: /customers", HttpStatus.OK);
+    }
+
+    @RequestMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout, Model model) {
+
+        if (error != null) {
+            model.addAttribute("error", "Wrong username or password!");
+        }
+        if (logout != null) {
+            model.addAttribute("msg", "You have successfully logged out!");
+        }
+        return "login";
     }
 }
